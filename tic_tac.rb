de40f895f2554a9f7ee45a016d82d53098152c33
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-10-17 13:32:51 -0500

    designing game play script

--- tic_tac.rb
+++ tic_tac.rb
@@ -1,8 +1,22 @@
+
+#BEHAVIOR
+module Play
+  def change_player
+   if player_toggle == true
+    player_toggle = false
+   elsif player_toggle == false
+     player_toggle = true 
+   end
+  end
+end
+
+#OBJECTS
 class Game
+  include Play
+  attr_reader :player_toggle
   attr_accessor :board, :player_one, :player_two
   def initialize
-    @player_one = []
-    @player_two = []
+    @player_toggle = true
     @board =[
      [1,2,3],
      [4,5,6],
@@ -19,22 +33,73 @@ class Game
     p ' - - + - - + - - '
     puts ''
   end  
-
 end 
 
-#BEHAVIOR
-module Play
-end
+
 
 #- - - - - - - - -
 #runner 
-tester_game = Game.new
-tester_game.display_new_board
-tester_game.board[0][0] = 'x'
-tester_game.board[0][1] = 'x'
-tester_game.board[0][2] = 'x'
-tester_game.display_new_board
 
-if tester_game.board[0].each {|place| place == 'x'}
-puts 'Won!'
-end
+puts 'WELCOME TO TIC TAC TOE!'
+puts 'DO YOU WANT TO PLAY? (y/n)'
+
+start_game = gets.chomp
+if start_game == 'y'
+ puts 'lets play!'
+ puts ''
+ puts ''
+ tester_game = Game.new
+ tester_game.display_new_board
+ puts ''
+ puts ''
+ puts ' - - - - - - - - - - - - -'
+ puts 'Make your move: PLAYER ONE' 
+ puts 'chose a square number: (1-9)'
+
+  move = gets.chomp
+   case move
+     when '1'
+       tester_game.board[0][0] = 'x'
+       tester_game.display_new_board
+     when '2'
+       tester_game.board[0][1] = 'x'
+       tester_game.display_new_board
+     when '3'
+       tester_game.board[0][2] = 'x'
+       tester_game.display_new_board
+     when '4'
+       tester_game.board[1][0] = 'x'
+       tester_game.display_new_board
+     when '5'
+       tester_game.board[1][1] = 'x'
+       tester_game.display_new_board
+     when '6'
+       tester_game.board[1][2] = 'x'
+       tester_game.display_new_board 
+     when '7'
+       tester_game.board[2][0] = 'x'
+       tester_game.display_new_board
+     when '8'
+       tester_game.board[2][1] = 'x'
+       tester_game.display_new_board
+     when '9'
+       tester_game.board[2][2] = 'x'
+       tester_game.display_new_board  
+
+    end 
+
+else
+ puts 'okay, we\'ll play later!' 
+end 
+
+
+
+# 1 tester_game.board[0][0] = 'x'
+# 2 tester_game.board[0][1] = 'x'
+# 3 tester_game.board[0][2] = 'x'
+# tester_game.whos_turn
+
+
+
+
+

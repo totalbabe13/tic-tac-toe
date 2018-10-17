8960073edd5ff64664628f111f8c7937c1c1766f
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-10-17 12:13:03 -0500

    initial commit, design for game board

---
+++ tic_tac.rb
@@ -0,0 +1,17 @@
+game_board = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9],
+]
+
+p '- - + - - + - -'
+p "- #{game_board[0][0]} | - #{game_board[0][1]} | - #{game_board[0][2]}"
+
+p '- - + - - + - -'
+p "- #{game_board[1][0]} | - #{game_board[1][1]} | - #{game_board[1][2]}"
+
+
+p '- - + - - + - -'
+p "- #{game_board[2][0]} | - #{game_board[2][1]} | - #{game_board[2][2]}"
+
+puts ''
